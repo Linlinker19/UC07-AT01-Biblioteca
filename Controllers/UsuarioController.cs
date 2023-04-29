edabedd7ab0c9231f0488c2bb2fcd1eda11e4315
Linlinker19 <linlinker19@gmail.com>
2023-04-29 13:38:31 -0400

    criação da classe Usuario, Views e Controller relativo a esta

---
+++ Controllers/UsuarioController.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Biblioteca.Models;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace Biblioteca.Controllers
+{
+    public class UsuarioController : Controller    {
+        
+    }
+}
