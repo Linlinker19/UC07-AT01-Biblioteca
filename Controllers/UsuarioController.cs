1742da4ee4ec05163dae59d5503b40fb5f4f258e
Linlinker19 <linlinker19@gmail.com>
2023-04-29 17:15:05 -0400

    ajuste de métodos em classes

--- Controllers/UsuarioController.cs
+++ Controllers/UsuarioController.cs
@@ -10,5 +10,49 @@ namespace Biblioteca.Controllers
 {
     public class UsuarioController : Controller    {
         
+        public IActionResult Cadastro()
+        {
+            
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Cadastro(Usuario u)
+        {
+            UsuarioService usuarioService = new UsuarioService();
+
+            if(u.Id == 0)
+            {
+                usuarioService.Inserir(u);
+            }
+            else
+            {
+                usuarioService.Atualizar(u);
+            }
+
+            return RedirectToAction("Listagem");
+        }
+
+        public IActionResult Listagem(string tipoFiltro, string filtro)
+        {
+            /* Autenticacao.CheckLogin(this); */
+            FiltrosUsuarios objFiltro = null;
+            if(!string.IsNullOrEmpty(filtro))
+            {
+                objFiltro = new FiltrosUsuarios();
+                objFiltro.Filtro = filtro;
+                objFiltro.TipoFiltro = tipoFiltro;
+            }
+            UsuarioService usuarioService = new UsuarioService();
+            return View(usuarioService.ListarTodos(objFiltro));
+        }
+
+        public IActionResult Edicao(int id)
+        {
+            /* Autenticacao.CheckLogin(this); */
+            UsuarioService us = new UsuarioService();
+            Usuario u = us.ObterPorId(id);
+            return View(u);
+        }
     }
 }
